758e60262ea073e051bed294a60fb2d2ef414741
miner <miner@Legion1.hitronhub.home>
2018-02-05 04:31:14 -0800

    Initial Commit

---
+++ client.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import socket, time, fcntl, os, select, datetime
+from miners import *
+import subprocess
+
+mining = 'None'
+TCP_IP = '192.168.0.23'
+TCP_PORT = 5000
+BUFFER_SIZE = 20
+
+register = "register %s" % socket.gethostname()
+update = "update %s" % mining
+response = { 
+                "welcome" : register,
+            }
+s = None
+
+def pipe_read(output):
+    fd = output.fileno()
+    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+    try:
+        return output.read()
+    except:
+        return ""
+
+def reconnect():
+    global s
+    retries = 1
+    while 1:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print "Socket successfully created."
+            s.connect((TCP_IP, TCP_PORT))
+            print "Connected to Server."
+            s.setblocking(0)
+            print "Set to non-blocking"
+            break
+        except socket.error as err:
+            print("Socket failed with error %s" %(err))
+            print("Retry #%d in 30 seconds." % retries)
+            retries += 1
+            time.sleep(30)
+reconnect()
+
+miner = None
+def mine(token):
+    global miner, mining
+    print("Switching miner")
+    if miner is not None:
+        try:
+            miner.kill()
+        except OSError: #Most likely already killed.
+            pass
+        try:
+            output, errors = miner.communicate()
+            print(output)
+            print(errors)
+        except ValueError: #Already ded.
+            pass
+    try:
+        tomine = mine_map[token]
+    except KeyError:
+        print("Got unknown token from server. %s" % token)
+    else:
+        miner = subprocess.Popen(tomine.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Now mining %s" % token)
+        mining = token
+    
+outbuf = []
+start_mining = False
+
+def socket_send(s, text):
+    try:
+        s.send(text)
+    except socket.error as e:
+        print("Server connection lost. Could not send %s.\n%s\nAttempting reconnect" % (text, e))
+        reconnect()
+
+def socket_recv(s, size):
+    try:
+        data = s.recv(size)
+    except socket.error as e:
+        print("Server connection lost. Unable to recv() data.\n%s\nAttempting reconnect." % e)
+        reconnect()
+        return ""
+    return data
+last_start = datetime.datetime.now()
+while 1:
+    ready = select.select([s], [], [], 200)
+    if ready[0]:
+        data = socket_recv(s, BUFFER_SIZE)
+
+        if data.startswith('mine'):
+            token = data[data.find(' '):].strip()
+            start_mining = True
+            print("Server updated us. Most profitable coin to mine is %s." % token)
+            if token == mining:
+                print("We are already minging %s" % token)
+            else:
+                mine(token)
+        elif data.startswith('stats'):
+            outbuf.append('stats %s' % mine_api[mining]())
+        elif data.startswith('restart'):
+            print("Server thinks we need to restart our miner.")
+            if last_start - datetime.datetime.now() < datetime.timedelta(minutes=5):
+                print("We restarted the miner less than 5 minutes ago. We shall wait.")
+            else:
+                mine(mining)
+        else:
+            try:
+                resp = response[data.lower().strip()]
+                outbuf.append(resp)
+            except KeyError:
+                print("Got unknown response from server. %s" % data)
+                #Check for disconnect.
+                socket_send(s, "update %s" % mining)
+    try:
+        msg = outbuf.pop(0)
+        s.send(msg)
+        
+    except IndexError:
+        pass # nothing to send
+    except socket.error as e:
+        print("Error connecting to server. %s\nAttempting reconnect." % e)
+        reconnect()
+    if start_mining and miner.poll() is not None:
+        print("Miner stopped, restarting.")
+        mine(mining)
+    elif miner:
+        out = pipe_read(miner.stdout)
+        err = pipe_read(miner.stderr)
+        if len(out.strip()) > 0:
+            print(out)
+        if len(err.strip()) > 0:
+            print(err)
+        
+        
+    time.sleep(1)
+s.close()
---
+++ miners.py
@@ -0,0 +1,70 @@
+import socket, urllib, json
+from device import *
+username          = 'LegionMetal'
+workername        = socket.gethostname()
+password              = 'legion1'
+
+ccminer = '/home/miner/miners/ccminer/ccminer'
+equihash = '/home/miner/miners/equihash/miner'
+neoscrypt = '/home/miner/miners/nsgminer/nsgminer'
+lyra2v2 = '/home/miner/miners/vertminer-nvidia/vertminer'
+
+
+# All miner configuration here.  Add/Change/Delete pairs as nessisary
+#Altcommunity
+#Bytecoin 
+BitcoinGold           = '%s --server us-east1.equihash-hub.miningpoolhub.com --user %s.%s --pass x --port 20595' % (equihash, username, workername)
+DGBGroestl           = '%s -a myr-gr -o stratum+tcp://hub.miningpoolhub.com:20499 -u %s.%s -p x' % (ccminer, username, workername)
+#Decred
+#DigitalNote
+#Electroneum
+Ethereum              = 'ethminer --opencl-device 0 -G -S us-east.ethash-hub.miningpoolhub.com:20535 -O %s.%s:x' % (username, workername)
+EthereumClassic       = 'ethminer --opencl-device 0 -G -S us-east.ethash-hub.miningpoolhub.com:20555 -O %s.%s:x' % (username, workername)
+Expanse               = 'ethminer --opencl-device 0 -G -S us-east.ethash-hub.miningpoolhub.com:20565 -O %s.%s:x' % (username, workername)
+Feathercoin           = '%s -o stratum+tcp://hub.miningpoolhub.com:20510 -u %s.%s -p x' % (neoscrypt, username, workername)
+GroestlCoin           = '%s -a groestl -o stratum+tcp://hub.miningpoolhub.com:20486 -u %s.%s -p x' % (ccminer, username, workername)
+#Halcyon
+#Hush
+#Karbowanec
+#Komodo
+#LBRY
+#Metaverse
+Monacoin              = '%s -o stratum+tcp://hub.miningpoolhub.com:20593 -u %s.%s -p x -d %s' % (lyra2v2, username, workername, device)
+Monero                = '%s -a cryptonight -o stratum+tcp://us-east.monero.miningpoolhub.com:20580 -u %s.%s -p x' % (ccminer, username, workername)
+Musicoin              = 'ethminer --opencl-device 0 -G -S us-east.ethash-hub.miningpoolhub.com:20585 -O %s.%s:x' % (username, workername)
+MyriadGroestl        = '%s -a myr-gr -o stratum+tcp://us-east1.myriadcoin-groestl.miningpoolhub.com:20479 -u %s.%s -p x' % (ccminer, username, workername)
+#Orbitcoin
+#PascalLite
+#Pascalcoin
+#Phoenixcoin
+#Pirl
+#Sibcoin
+#Soilcoin
+#Sumokoin
+#Ubiq
+Vertcoin              = '%s -o stratum+tcp://hub.miningpoolhub.com:20507 -u %s.%s -p x -d %s' % (lyra2v2, username, workername, device)
+Zcash                 = '%s --server us-east1.zcash.miningpoolhub.com --user %s.%s --pass x --port 20570 --api' % (equihash, username, workername)
+Zclassic              = '%s --server us-east.equihash-hub.miningpoolhub.com --user %s.%s --pass x --port 20575 --api' % (equihash, username, workername)
+Zencash               = '%s --server us-east.equihash-hub.miningpoolhub.com --user %s.%s --pass x --port 20594 --api' % (equihash, username, workername)
+
+def equihash_api():
+    try:
+        response = urllib.urlopen('http://127.0.0.1:42000/getstat')
+    except:
+        print("Unable to connect to miner API")
+        return "{}"
+    return response.read()
+
+mine_map = { 'ZCL': Zclassic,
+             'ZEN': Zencash,
+             'BTG': BitcoinGold,
+             'ZEC': Zcash,
+             'MONA': Monacoin,
+             'VTC': Vertcoin,
+             'ETH': Ethereum,
+             'FTC': Feathercoin, }
+
+mine_api = { 'ZCL': equihash_api,
+             'ZEC': equihash_api,
+             'ZEN': equihash_api,
+           }
